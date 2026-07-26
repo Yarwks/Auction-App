a91262e3c804f22939aef4c90ebc77c1450c4ec3
Ray Wekesa <ray.wekesa@student.moringaschool.com>
2026-07-26 22:42:17 +0300

    Fix syntax error in REST_FRAMEWORK settings in settings.py

--- Django-project/djangobackend/djangobackend/settings.py
+++ Django-project/djangobackend/djangobackend/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
 }
 
 SIMPLE_JWT = {
