211d792c7a68f40c3bf07b5000fb436fcbfa112b
Ray Wekesa <ray.wekesa@student.moringaschool.com>
2026-07-26 17:43:39 +0300

    Fixed the api url

--- Frontend-Auction-App/AuctionApp/src/api/axios.js
+++ Frontend-Auction-App/AuctionApp/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://auction-app-wby3.onrender.com',
+  baseURL: 'https://auction-app-wby3.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
